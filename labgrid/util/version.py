aea252a617feba7f61d3f434d4f45b45094c7741
Jan Luebbe <jlu@pengutronix.de>
2024-06-11 20:57:07 +0200

    util/version: enable caching for labgrid_version()
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- labgrid/util/version.py
+++ labgrid/util/version.py
@@ -1,8 +1,9 @@
 """
 This module contains helper functions for working with version.
 """
+from functools import cache
 
-
+@cache
 def labgrid_version():
     import contextlib
     from importlib.metadata import PackageNotFoundError, version
